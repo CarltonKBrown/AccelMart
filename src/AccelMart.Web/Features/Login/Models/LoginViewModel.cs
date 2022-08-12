8acc9f4c9c3dda1a9782a34262d5b7c7a4c5263c
Carlton Brown <carltonkbrown@outlook.com>
2022-08-12 11:23:42 -0500

    Started Login page to test EditForm Blazor component. Will complete once API has been implemented.

---
+++ src/AccelMart.Web/Features/Login/Models/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccelMart.Web.Features.Login.Models
+{
+    public class LoginViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email address is required!")]
+        [EmailAddress(ErrorMessage ="Invalid email format detected")]
+        [MinLength(10, ErrorMessage = "Email address is too short!")]
+        public string? Email { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required!")]
+        [MinLength(8, ErrorMessage = "Password is too short!")]
+        public string? Password { get; set; }
+    }
+}
